0b3fde9c881d5475aff7d49929c60ab668165896
cyrilcolot <32701611+cyrilcolot@users.noreply.github.com>
2020-08-02 18:01:46 +0200

    Debut

---
+++ DLivres/src/main/java/com/spring/henallux/DLivres/DLivresApplication.java
@@ -0,0 +1,13 @@
+package com.spring.henallux.DLivres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DLivresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DLivresApplication.class, args);
+	}
+
+}
---
+++ DLivres/src/test/java/com/spring/henallux/DLivres/DLivresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.henallux.DLivres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DLivresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
